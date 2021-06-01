1faa83720ddbfdc50a63589715746e4f19d27e80
hokkyss <hokkyss2@gmail.com>
2021-06-01 20:42:35 +0700

    finishing

--- src/app.py
+++ src/app.py
@@ -1,40 +1,85 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, request, jsonify
 
 from sorts import Sorter
-from utility import init, insert, get_content, html_table
+from utility import convert_BLOB_to_list, convert_list_to_BLOB, init, insert, get_content, html_table, preprocess
 
-selection_sort = Sorter.selection
-insertion_sort = Sorter.insertion
-merge_sort = Sorter.merge
-bubble_sort = Sorter.bubble
+selection = Sorter.selection
+merge = Sorter.merge
+bubble = Sorter.bubble
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 init()
 
-@app.route("/")
-def home():
-    return "";
-#    return render_template('home.html')
-
-@app.route("/sort/<type>", methods=["POST", "GET"])
-def sort(type):
-#    csvFile = request.files["file"]
+@app.route("/sort/selection", methods=["POST"])
+def selection_sort():
+    csvFile = request.files["file"]
     pivot = request.args.get("col")
     orientation = request.args.get("sort")
-    if request.method == "GET":
-        return render_template('index.html', type=type, col=pivot, sort=orientation)
-    else:
-        csvFile = request.form["file"]
-        return ""
+
+    file = open(csvFile, "rb")
+    blob = file.read()
+    data = convert_BLOB_to_list(blob)
+    data = preprocess(data, pivot)
+
+    try:
+        result, execution_time = selection(data, pivot, orientation)
+    except:
+        return jsonify(Error="Index invalid!")
+
+    result = convert_list_to_BLOB(result)
+    insert(result, execution_time, "selection")
+
+@app.route("/sort/bubble", methods=["POST"])
+def bubble_sort():
+    csvFile = request.files["file"]
+    pivot = request.args.get("col")
+    orientation = request.args.get("sort")
+
+    file = open(csvFile, "rb")
+    blob = file.read()
+    data = convert_BLOB_to_list(blob)
+    data = preprocess(data, pivot)
+
+    try:
+        result, execution_time = bubble(data, pivot, orientation)
+    except:
+        return jsonify(Error="Index invalid!")
+
+    result = convert_list_to_BLOB(result)
+    insert(result, execution_time, "bubble")
+
+@app.route("/sort/merge", methods=["POST"])
+def merge_sort():
+    csvFile = request.files["file"]
+    pivot = request.args.get("col")
+    orientation = request.args.get("sort")
+
+    file = open(csvFile, "rb")
+    blob = file.read()
+    data = convert_BLOB_to_list(blob)
+    data = preprocess(data, pivot)
+
+    try:
+        result, execution_time = merge(data, pivot, orientation)
+    except:
+        return jsonify(Error="Index invalid!")
+
+    result = convert_list_to_BLOB(result)
+    insert(result, execution_time, "merge")
 
 @app.route("/sort/result", methods=["GET"])
 def result():
     ID = request.args.get("id")
+    temp = ""
     if ID == None:
-        return html_table(get_content())
+        temp = html_table(get_content())
     else:
-        return html_table(get_content(ID=ID))
+        temp = html_table(get_content(ID=ID))
+    
+    if temp == None:
+        return jsonify(Error="ID not found!")
+    return temp
 
 if __name__ == '__main__':
     app.run()
--- src/utility.py
+++ src/utility.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
+# database Utility
 def init():
     database = mysql.connect(
         host="localhost",
@@ -15,9 +16,9 @@ def init():
 
     cursor.execute("CREATE DATABASE IF NOT EXISTS SortAPI")
     cursor.execute("USE SortAPI")
-    cursor.execute("CREATE TABLE IF NOT EXISTS sorts (ID INT AUTO_INCREMENT PRIMARY KEY, Waktu TIMESTAMP, Algoritma VARCHAR(9) DEFAULT 'selection', Result BLOB, ExecutionTime DOUBLE)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS sorts (ID INT AUTO_INCREMENT PRIMARY KEY, Waktu TIMESTAMP DEFAULT current_timestamp(), Algoritma VARCHAR(9) DEFAULT 'selection', Result BLOB, ExecutionTime DOUBLE)")
 
-def insert(Waktu, Result, ExecutionTime, Algoritma="selection"):
+def insert(Result, ExecutionTime, Algoritma):
     database = mysql.connect(
         host="localhost",
         user=os.getenv("MYSQL_USER"),
@@ -27,8 +28,8 @@ def insert(Waktu, Result, ExecutionTime, Algoritma="selection"):
     database.autocommit = True
     cursor = database.cursor()
 
-    query = "INSERT INTO sorts (Waktu, Algoritma, Result, ExecutionTime) VALUES (?, ?, ?, ?)"
-    query_tuple = (Waktu, Algoritma, Result, ExecutionTime)
+    query = "INSERT INTO sorts (Algoritma, Result, ExecutionTime) VALUES (?, ?, ?, ?)"
+    query_tuple = (Algoritma, Result, ExecutionTime)
     cursor.execute(query, query_tuple)
 
     print(cursor.rowcount, "record inserted.")
@@ -50,20 +51,74 @@ def get_content(ID=-1):
         query = query + "WHERE ID=" + str(ID)
     cursor.execute(query)
 
-    return cursor.fetchone();
+    res = cursor.fetchone()
+
+    return convert_BLOB_to_list(res)
+
+
+# Other functional utility
+def is_number(i):
+    try:
+        int(i)
+        return True
+    except:
+        return False
+
+def preprocess(data, pivot):
+    is_header = True
+    is_first = True
+    is_a_number = True
+
+    invalid_row = []
+
+    for row in data:
+        if is_header:
+            is_header = False
+            continue
+        if is_first:
+            is_a_number = is_number(row[pivot])
+            is_first = False
+            continue
+        
+        is_number_type = is_number(row[pivot])
+        if (is_a_number and is_number_type) or (not is_a_number and not is_number_type):
+            continue
+        invalid_row.append(row)
+    
+    for invalid in invalid_row:
+        data.remove(invalid)
+    
+    return data
+
+def convert_list_to_BLOB(data):
+    text = 'b"'
+    data = [['a', 'b', 'c'], [1, 2, 3], [4, 5, 6]]
+
+    for i in range(len(data)):
+        data[i] = ",".join(data[i])
+    
+    text = text + "\r\n".join(data)
+    text = text + '"'
+    return text
 
 def convert_BLOB_to_list(BLOB):
     '''
     convert a csv file into list of list
 
-    i.e. csv content (as a blob): b"a,b,c\r\n1,2,3\r\n4,5,6"
+    i.e. 
+    csv content (as a blob): b"a,b,c\r\n1,2,3\r\n4,5,6"
+    
+    after decoding, the content is
+    a,b,c
+    1,2,3
+    4,5,6
+
     returns [['a', 'b', 'c'], ['1', '2', '3'], ['4', '5', '6']]
     '''
     if BLOB == None:
         return []
 
     text = BLOB.decode("UTF8")
-    print(text)
 
     each_line = text.split("\n")
     
@@ -75,11 +130,13 @@ def convert_BLOB_to_list(BLOB):
     
     return each_line
 
-def html_table(BLOB):
+def html_table(content):
+    if content == []:
+        return None
+
     convert_body = lambda column : "<td>" + column + "</td>\n"
     convert_header = lambda column : "<th>" + column + "</th>\n" 
 
-    content = convert_BLOB_to_list(BLOB)
     html = "<table>\n"
 
     is_header = True
@@ -93,6 +150,4 @@ def html_table(BLOB):
         html = html + "</tr>\n"
 
     html = html + "</table>"
-
-    print(html)
     return html
